969c106146a5b5ac3fb62f388d775f382f1c4576
alin.ciobanu <alin.ciobanu@cti.pub.ro>
2014-05-03 00:02:18 +0300

    prepared some algorithm for solving -- not tested

--- js/services/custom-services.js
+++ js/services/custom-services.js
@@ -27,7 +27,9 @@ APP
                 DOWN: 2,
                 RIGHT: 3,
                 LEFT: 4
-             }
+             },
+            MAX_DEPTH: 6,
+            INFINITY: 999999
         };
 
         return settings;
@@ -72,165 +74,40 @@ APP
 
         };
 
-        var moveLeft = function () {
-
-            for (var i = 0; i < thisService.boardSize; i++) {
-                var line = thisService.board[i];
-                var lastNonZeroIndex = -1;
-                var currentCompare = {
-                    index: -1,
-                    value: -1
-                };
-
-                for (var j = 0; j < line.length; j++) {
-                    if (line[j] != AppSettings.constants.EMPTY) {
-                        if (line[j] == currentCompare.value) {
-                            line[currentCompare.index] *= 2;
-                            line[j] = AppSettings.constants.EMPTY;
-                        }
-                        currentCompare.value = line[j];
-                        currentCompare.index = j;
-                        lastNonZeroIndex = j;
-                    }
-                }
-
-                for (var j = 0; j < line.length; j++) {
-                    while (line[j] == AppSettings.constants.EMPTY && j < lastNonZeroIndex) {
-                        for (var k = j; k < line.length - 1; k++) {
-                            line[k] = line[k + 1];
-                        }
-                        line[line.length - 1] = AppSettings.constants.EMPTY;
-                        lastNonZeroIndex--;
-                    }
-                }
-            }
-
-        };
-
-        var moveUp = function () {
-
-            for (var i = 0; i < thisService.boardSize; i++) {
-                var lastNonZeroIndex = -1;
-                var currentCompare = {
-                    index: -1,
-                    value: -1
-                };
-
-                for (var j = 0; j < thisService.boardSize; j++) {
-                    if (thisService.board[j][i] != AppSettings.constants.EMPTY) {
-                        if (thisService.board[j][i] == currentCompare.value) {
-                            thisService.board[currentCompare.index][i] *= 2;
-                            thisService.board[j][i] = AppSettings.constants.EMPTY;
-                        }
-                        currentCompare.value = thisService.board[j][i];
-                        currentCompare.index = j;
-                        lastNonZeroIndex = j;
-                    }
-                }
-
-                for (var j = 0; j < thisService.boardSize; j++) {
-                    while (thisService.board[j][i] == AppSettings.constants.EMPTY && j < lastNonZeroIndex) {
-                        for (var k = j; k < thisService.boardSize - 1; k++) {
-                            thisService.board[k][i] = thisService.board[k + 1][i];
-                        }
-                        thisService.board[thisService.boardSize - 1][i] = AppSettings.constants.EMPTY;
-                        lastNonZeroIndex--;
-                    }
-                }
-            }
-
-        };
-
-        var moveRight = function () {
-
-            for (var i = 0; i < thisService.boardSize; i++) {
-                var line = thisService.board[i];
-                var lastNonZeroIndex = -1;
-                var currentCompare = {
-                    index: -1,
-                    value: -1
-                };
-
-                for (var j = line.length - 1; j >= 0; j--) {
-                    if (line[j] != AppSettings.constants.EMPTY) {
-                        if (line[j] == currentCompare.value) {
-                            line[currentCompare.index] *= 2;
-                            line[j] = AppSettings.constants.EMPTY;
-                        }
-                        currentCompare.value = line[j];
-                        currentCompare.index = j;
-                        lastNonZeroIndex = j;
-                    }
-                }
-
-                for (var j = line.length - 1; j >= 0; j--) {
-                    while (line[j] == AppSettings.constants.EMPTY && j > lastNonZeroIndex) {
-                        for (var k = j; k >= 0; k--) {
-                            line[k] = line[k - 1];
-                        }
-                        line[0] = AppSettings.constants.EMPTY;
-                        lastNonZeroIndex++;
-                    }
-                }
-            }
-
-        };
-
-        var moveDown = function () {
-
-            for (var i = 0; i < thisService.boardSize; i++) {
-                var lastNonZeroIndex = -1;
-                var currentCompare = {
-                    index: -1,
-                    value: -1
-                };
-
-                for (var j = thisService.boardSize - 1; j >= 0; j--) {
-                    if (thisService.board[j][i] != AppSettings.constants.EMPTY) {
-                        if (thisService.board[j][i] == currentCompare.value) {
-                            thisService.board[currentCompare.index][i] *= 2;
-                            thisService.board[j][i] = AppSettings.constants.EMPTY;
-                        }
-                        currentCompare.value = thisService.board[j][i];
-                        currentCompare.index = j;
-                        lastNonZeroIndex = j;
-                    }
-                }
-
-                for (var j = thisService.boardSize - 1; j >= 0; j--) {
-                    while (thisService.board[j][i] == AppSettings.constants.EMPTY && j > lastNonZeroIndex) {
-                        for (var k = j; k > 0; k--) {
-                            thisService.board[k][i] = thisService.board[k - 1][i];
-                        }
-                        thisService.board[0][i] = AppSettings.constants.EMPTY;
-                        lastNonZeroIndex++;
-                    }
-                }
-            }
-
-        };
-
         thisService.move = function (direction) {
+            BoardUtilsService.move(direction, thisService.board, thisService.boardSize);
+        };
 
-            switch(direction) {
+        var evaluateBoard = function (board, size) {
+            return 0;
+        };
 
-                case AppSettings.constants.directions.RIGHT:
-                    moveRight();
-                    break;
+        thisService.optimumNextMove = function (depth, board, size) {
 
-                case AppSettings.constants.directions.LEFT:
-                    moveLeft();
-                    break;
-
-                case AppSettings.constants.directions.UP:
-                    moveUp();
-                    break;
-
-                case AppSettings.constants.directions.DOWN:
-                    moveDown();
-                    break;
-
+            if (depth == 0) {
+                return evaluateBoard(board, size);
             }
+
+            var freePositions = BoardUtilsService.getEmptyPositions(board, size);
+            var max = -AppSettings.constants.INFINITY;
+
+            for (var i = 0; i < freePositions.length; i++) {
+                var freePos = freePositions[i];
+                var newBoard = BoardUtilsService.matrixCopy(board, size);
+                var score;
+                BoardUtilsService.markAPosition(newBoard, BoardUtilsService.randomTileValue(), freePos);
+                for (var j in AppSettings.constants.directions) {
+                    var direction = AppSettings.constants.directions[j];
+                    var newBoardDirections = BoardUtilsService.matrixCopy(newBoard, size);
+                    BoardUtilsService.move(direction, newBoardDirections, size);
+                    score = thisService.optimumNextMove(depth - 1, newBoardDirections, size);
+                    if (score > max) {
+                        max = score;
+                    }
+                }
+            }
+
+            return max;
 
         };
 
@@ -241,6 +118,10 @@ APP
     .factory('BoardUtilsService', function (AppSettings){
 
         var thisService = {};
+
+        thisService.markAPosition = function (board, value, position) {
+            board[position.line][position.col] = value;
+        };
 
         thisService.randomEmptyPosition = function (board, size) {
 
@@ -297,6 +178,184 @@ APP
             }
 
             return emptyPositions;
+
+        };
+
+        var moveLeft = function (board, size) {
+
+            for (var i = 0; i < size; i++) {
+                var line = board[i];
+                var lastNonZeroIndex = -1;
+                var currentCompare = {
+                    index: -1,
+                    value: -1
+                };
+
+                for (var j = 0; j < line.length; j++) {
+                    if (line[j] != AppSettings.constants.EMPTY) {
+                        if (line[j] == currentCompare.value) {
+                            line[currentCompare.index] *= 2;
+                            line[j] = AppSettings.constants.EMPTY;
+                        }
+                        currentCompare.value = line[j];
+                        currentCompare.index = j;
+                        lastNonZeroIndex = j;
+                    }
+                }
+
+                for (var j = 0; j < line.length; j++) {
+                    while (line[j] == AppSettings.constants.EMPTY && j < lastNonZeroIndex) {
+                        for (var k = j; k < line.length - 1; k++) {
+                            line[k] = line[k + 1];
+                        }
+                        line[line.length - 1] = AppSettings.constants.EMPTY;
+                        lastNonZeroIndex--;
+                    }
+                }
+            }
+
+        };
+
+        var moveUp = function (board, size) {
+
+            for (var i = 0; i < size; i++) {
+                var lastNonZeroIndex = -1;
+                var currentCompare = {
+                    index: -1,
+                    value: -1
+                };
+
+                for (var j = 0; j < size; j++) {
+                    if (board[j][i] != AppSettings.constants.EMPTY) {
+                        if (board[j][i] == currentCompare.value) {
+                            board[currentCompare.index][i] *= 2;
+                            board[j][i] = AppSettings.constants.EMPTY;
+                        }
+                        currentCompare.value = board[j][i];
+                        currentCompare.index = j;
+                        lastNonZeroIndex = j;
+                    }
+                }
+
+                for (var j = 0; j < size; j++) {
+                    while (board[j][i] == AppSettings.constants.EMPTY && j < lastNonZeroIndex) {
+                        for (var k = j; k < boardSize - 1; k++) {
+                            board[k][i] = board[k + 1][i];
+                        }
+                        board[size - 1][i] = AppSettings.constants.EMPTY;
+                        lastNonZeroIndex--;
+                    }
+                }
+            }
+
+        };
+
+        var moveRight = function (board, size) {
+
+            for (var i = 0; i < size; i++) {
+                var line = board[i];
+                var lastNonZeroIndex = -1;
+                var currentCompare = {
+                    index: -1,
+                    value: -1
+                };
+
+                for (var j = line.length - 1; j >= 0; j--) {
+                    if (line[j] != AppSettings.constants.EMPTY) {
+                        if (line[j] == currentCompare.value) {
+                            line[currentCompare.index] *= 2;
+                            line[j] = AppSettings.constants.EMPTY;
+                        }
+                        currentCompare.value = line[j];
+                        currentCompare.index = j;
+                        lastNonZeroIndex = j;
+                    }
+                }
+
+                for (var j = line.length - 1; j >= 0; j--) {
+                    while (line[j] == AppSettings.constants.EMPTY && j > lastNonZeroIndex) {
+                        for (var k = j; k >= 0; k--) {
+                            line[k] = line[k - 1];
+                        }
+                        line[0] = AppSettings.constants.EMPTY;
+                        lastNonZeroIndex++;
+                    }
+                }
+            }
+
+        };
+
+        var moveDown = function (board, size) {
+
+            for (var i = 0; i < size; i++) {
+                var lastNonZeroIndex = -1;
+                var currentCompare = {
+                    index: -1,
+                    value: -1
+                };
+
+                for (var j = size - 1; j >= 0; j--) {
+                    if (board[j][i] != AppSettings.constants.EMPTY) {
+                        if (board[j][i] == currentCompare.value) {
+                            board[currentCompare.index][i] *= 2;
+                            board[j][i] = AppSettings.constants.EMPTY;
+                        }
+                        currentCompare.value = board[j][i];
+                        currentCompare.index = j;
+                        lastNonZeroIndex = j;
+                    }
+                }
+
+                for (var j = size - 1; j >= 0; j--) {
+                    while (board[j][i] == AppSettings.constants.EMPTY && j > lastNonZeroIndex) {
+                        for (var k = j; k > 0; k--) {
+                            board[k][i] = board[k - 1][i];
+                        }
+                        board[0][i] = AppSettings.constants.EMPTY;
+                        lastNonZeroIndex++;
+                    }
+                }
+            }
+
+        };
+
+        thisService.move = function (direction, board, size) {
+
+            switch(direction) {
+
+                case AppSettings.constants.directions.RIGHT:
+                    moveRight(board, size);
+                    break;
+
+                case AppSettings.constants.directions.LEFT:
+                    moveLeft(board, size);
+                    break;
+
+                case AppSettings.constants.directions.UP:
+                    moveUp(board, size);
+                    break;
+
+                case AppSettings.constants.directions.DOWN:
+                    moveDown(board, size);
+                    break;
+
+            }
+
+        };
+
+
+        thisService.matrixCopy = function (board, size) {
+
+            var bCopy = [];
+
+            for (var i = 0; i < size; i++) {
+                bCopy.push([]);
+                for (var j = 0; j < size; j++) {
+                    bCopy[i].push(board[i][j]);
+                }
+            }
+
+            return bCopy;
 
         };
 
